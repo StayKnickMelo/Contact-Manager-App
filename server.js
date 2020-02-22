479a4a2f3dcda39f380402565837a8dbca30b0aa
Igor Dubov <dubovigor1992@gmail.com>
2020-02-22 21:55:45 +1000

    deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 
 const app = express();
 
@@ -9,12 +11,19 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extend: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome To The Contact Keeper API' }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Production
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
